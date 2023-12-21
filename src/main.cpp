d8053ea135cba1c4e28f3abca00e506b441f17ef
rTaiShan <raul.tai@hotmail.com>
2023-12-21 19:32:00 -0300

    Pin setup moved into initializePins()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include <enc4094.h>
 #include <programSelect.h>
-
+#include <pins.h>
 #define BAUDRATE 9600
 
+void initializePins();
+
 void setup() {
-  getFavoritePatch();
-  // My pro-micro transmits at 2x the baud rate, for some reason
-  Serial.begin(BAUDRATE/2); 
+  getFavoritePatch(); // Fetch selected patch from internal EEPROM
+  Serial.begin(BAUDRATE/2); // My pro-micro transmits at 2x the baud rate, for some reason
+  initializePins();
+  handleSIPOEncoder(); // Send data to CD4094
+}
+
+void loop() {
+  handleEncoderButton();
+  updateRotary();
+  handleSIPOEncoder();
+}
+
+void initializePins() { 
   // Initializing Inputs
   pinMode(ROTARYCLOCKPIN, INPUT_PULLUP);
   pinMode(ROTARYDATAPIN, INPUT_PULLUP);
@@ -17,12 +29,4 @@ void setup() {
   pinMode(SIPODATAPIN, OUTPUT);
   pinMode(SIPOCLOCKPIN, OUTPUT);
   pinMode(SIPOSTROBEPIN, OUTPUT);
-  // Send data to CD4094
-  handleSIPOEncoder();
-}
-
-void loop() {
-  handleEncoderButton();
-  updateRotary();
-  handleSIPOEncoder();
 }
